6745ccb6ea1c366a408261610346e60cac2fb0d1
Adnanshaikh10101 <shaikhaadnan02@gmail.com>
2026-05-21 14:36:00 +0530

    Update Contact.jsx

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -21,7 +21,7 @@ function Contact() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/contact", {
+      const res = await fetch("https://wordconverter.onrender.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
